3484d67c93278fa597c3c15a8c99a1b801d31afb
Rafael Corsi <rafael.corsi@insper.edu.br>
2025-02-27 18:44:54 -0300

    Update comb_modules.py

--- comb_modules.py
+++ comb_modules.py
@@ -44,7 +44,7 @@ def exe2(q, a, b, c):
 
     @always_comb
     def comb():
-        q.next = a or b
+         q.next = ((not a) and (not b) and (not c)) or ((not a ) and b and c) or (a and (not b) and (not c)) or (a and b and c)
 
     return instances()
 
